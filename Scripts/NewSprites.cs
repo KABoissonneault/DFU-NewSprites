d943d36aa0869dfff9ce7315abef2b37bc349e89
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2021-07-03 08:47:21 -0400

    Added necromancer sprites

--- Scripts/NewSprites.cs
+++ Scripts/NewSprites.cs
@@ -12,6 +12,12 @@ using DaggerfallWorkshop.Game.Questing;
 using DaggerfallWorkshop.Game.Entity;
 using DaggerfallWorkshop.Game.Formulas;
 using DaggerfallWorkshop.Game.Items;
+using Wenzil.Console;
+using DaggerfallConnect.Arena2;
+using DaggerfallConnect.Utility;
+using DaggerfallWorkshop.Utility.AssetInjection;
+using System.IO;
+using System.Xml;
 
 namespace NewSpritesMod
 {
@@ -20,6 +26,7 @@ namespace NewSpritesMod
         private static Mod mod;
 
         public const int DruidCareerIndex = 147;
+        public const int NecromancerCareerIndex = 148;
 
         [Invoke(StateManager.StateTypes.Start, 0)]
         public static void Init(InitParams initParams)
@@ -28,6 +35,8 @@ namespace NewSpritesMod
 
             var go = new GameObject(mod.Title);
             go.AddComponent<NewSprites>();
+
+            ConsoleCommandsDatabase.RegisterCommand("export_texture_properties", "Exports the properties for all the records of the texture as XML", "EXPORT_TEXTURE_PROPERTIES <id> <out_id>", ExportTextureProperties);
 
             mod.IsReady = true;
         }
@@ -54,7 +63,6 @@ namespace NewSpritesMod
                 ParrySounds = false,
                 MapChance = 3,
                 LootTableKey = "U",
-                Weight = 350,
                 CastsMagic = true,
                 PrimaryAttackAnimFrames = new int[] { 0, 1, -1, 3, 2, 1, 0, -1, 5, 4, 0 },
                 ChanceForAttack2 = 33,
@@ -99,6 +107,68 @@ namespace NewSpritesMod
                 AcuteHearing = true,
             });
 
+            ArrayUtility.Add(ref EnemyBasics.Enemies, new MobileEnemy()
+            {
+                ID = NecromancerCareerIndex,
+                Behaviour = MobileBehaviour.General,
+                Affinity = MobileAffinity.Human,
+                MaleTexture = 1002,
+                FemaleTexture = 1002,
+                CorpseTexture = EnemyBasics.CorpseTexture(380, 1),
+                HasIdle = true,
+                HasRangedAttack1 = false,
+                HasRangedAttack2 = false,
+                CanOpenDoors = true,
+                MoveSound = (int)SoundClips.EnemyHumanMove,
+                BarkSound = (int)SoundClips.EnemyHumanBark,
+                AttackSound = (int)SoundClips.EnemyHumanAttack,
+                ParrySounds = false,
+                MapChance = 3,
+                LootTableKey = "U",
+                CastsMagic = true,
+                PrimaryAttackAnimFrames = new int[] { 0, 1, -1, 3, 2, 1, 0, -1, 5, 4, 0 },
+                ChanceForAttack2 = 33,
+                PrimaryAttackAnimFrames2 = new int[] { 0, 1, -1, 3, 2, 1, 0 },
+                ChanceForAttack3 = 33,
+                PrimaryAttackAnimFrames3 = new int[] { 0, -1, 5, 4, 0 },
+                HasSpellAnimation = true,
+                SpellAnimFrames = new int[] { 0, 1, 2, 3, 3 },
+                Team = MobileTeams.Undead,
+            });
+
+            DaggerfallEntity.CustomCareerTemplates.Add(NecromancerCareerIndex, new DFCareer()
+            {
+                Name = "Necromancer",
+                HitPointsPerLevel = 6,
+                Strength = 35,
+                Intelligence = 65,
+                Willpower = 60,
+                Agility = 45,
+                Endurance = 60,
+                Personality = 40,
+                Speed = 50,
+                Luck = 45,
+                PrimarySkill1 = DFCareer.Skills­.Mysticism,
+                PrimarySkill2 = DFCareer.Skills.Restoration,
+                PrimarySkill3 = DFCareer.Skills.Destruction,
+                MajorSkill1 = DFCareer.Skills.ShortBlade,
+                MajorSkill2 = DFCareer.Skills.Streetwise,
+                MajorSkill3 = DFCareer.Skills.Medical,
+                MinorSkill1 = DFCareer.Skills.Daedric,
+                MinorSkill2 = DFCareer.Skills.Thaumaturgy,
+                MinorSkill3 = DFCareer.Skills.BluntWeapon,
+                MinorSkill4 = DFCareer.Skills.CriticalStrike,
+                MinorSkill5 = DFCareer.Skills.Dragonish,
+                MissileWeapons = DFCareer.Proficiency.Forbidden,
+                UndeadAttackModifier = DFCareer.AttackModifier.Bonus,
+                ForbiddenShields = DFCareer.ShieldFlags.TowerShield | DFCareer.ShieldFlags.KiteShield | DFCareer.ShieldFlags.RoundShield,
+                ForbiddenArmors = DFCareer.ArmorFlags.Plate | DFCareer.ArmorFlags.Chain,
+                ForbiddenProficiencies = DFCareer.ProficiencyFlags.MissileWeapons,
+                SpellPointMultiplier = DFCareer.SpellPointMultipliers.Times_2_00,
+                SpellPointMultiplierValue = 2.0f,
+                DamageFromHolyPlaces = true,
+            });
+
             EnemyEntity.OnLootSpawned += OnEnemySpawned;
         }
 
@@ -108,10 +178,15 @@ namespace NewSpritesMod
             if (enemyEntity == null)
                 return;
 
-            if (enemyEntity.CareerIndex == DruidCareerIndex)
+            switch(enemyEntity.CareerIndex)
             {
-                SetupDruid(enemyEntity, args);
-            }            
+                case DruidCareerIndex:
+                    SetupDruid(enemyEntity, args);
+                    break;
+                case NecromancerCareerIndex:
+                    SetupNecromancer(enemyEntity, args);
+                    break;
+            }     
         }
 
         static byte[] OrcShamanSpells = { 0x06, 0x07, 0x16, 0x19, 0x1F };
@@ -220,5 +295,70 @@ namespace NewSpritesMod
             enemyEntity.Items.AddItem(item);
             enemyEntity.UpdateEquippedArmorValues(item, equipping: true);
         }
+
+        void SetupNecromancer(EnemyEntity enemyEntity, EnemyLootSpawnedEventArgs args)
+        {
+
+        }
+
+        static string ExportTextureProperties(string[] args)
+        {
+            if (args.Length < 2)
+                return "usage: EXPORT_TEXTURE_PROPERTIES <id> <out_id>";
+
+            if (!int.TryParse(args[0], out int inputArchive))
+                return "error: <id> must be an integer";
+
+            TextureFile texture = new TextureFile();
+            if (!texture.Load(Path.Combine(DaggerfallUnity.Instance.Arena2Path, TextureFile.IndexToFileName(inputArchive)), FileUsage.UseMemory, true))
+                return $"error: Could not open archive {inputArchive}";
+
+            // Ensure writeable
+            Directory.CreateDirectory(mod.PersistentDataDirectory);
+            Directory.CreateDirectory(Path.Combine(mod.PersistentDataDirectory, "Textures"));
+
+            int recordCount = texture.RecordCount;
+            for(int record = 0; record < recordCount; ++record)
+            {
+                DFSize scale = texture.GetScale(record);
+                DFPosition offset = texture.GetOffset(record);
+
+                bool hasCustomScale = scale.Width != 0 || scale.Height != 0;
+                bool hasCustomOffset = offset.X != 0 || offset.Y != 0;
+                if (hasCustomScale || hasCustomOffset)
+                {
+                    string xmlPath = Path.Combine(mod.PersistentDataDirectory, "Textures", $"{args[1]}_{record}-0.xml");
+
+                    using (FileStream outXml = new FileStream(xmlPath, FileMode.OpenOrCreate))
+                    using (XmlWriter xmlWriter = XmlWriter.Create(outXml))
+                    {
+                        xmlWriter.WriteStartElement("info");
+
+                        if (hasCustomScale)
+                        {
+                            float normX = 1 + scale.Width / 256.0f;
+                            float normY = 1 + scale.Height / 256.0f;
+
+                            
+                            xmlWriter.WriteElementString("scaleX", normX.ToString());
+                            xmlWriter.WriteElementString("scaleY", normY.ToString());
+                            
+                        }
+
+                        if(hasCustomOffset)
+                        {
+                            xmlWriter.WriteElementString("offsetX", offset.X.ToString());
+                            xmlWriter.WriteElementString("offsetY", offset.Y.ToString());
+                        }
+
+                        xmlWriter.WriteEndElement();
+                    }
+                }
+            }
+
+            return $"Properties exported to {mod.PersistentDataDirectory}";
+        }
+            
+
     }
 }
